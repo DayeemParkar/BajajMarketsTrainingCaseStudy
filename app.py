20017ab6eef832e6192d1967254de32e4c2e950d
DayeemParkar <dayeem59@gmail.com>
2023-02-07 17:18:25 +0530

    Token timestamp, login verification

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 '''This file contains the app'''
 
 # for running app
-from flask import Flask, session, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 # for swagger ui
 from flask_swagger_ui import get_swaggerui_blueprint
-# for helper functions, logger, Token class, PasswordHash class, DB, config vars
+# for helper functions, flask forms, logger, Token class, PasswordHash class, DB, config vars
 from helpers import *
-# for flask forms
-from forms import CustomerForm, AccountForm, LoginForm
 
 
 app = Flask(__name__)
@@ -22,27 +20,18 @@ SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
 )
 app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix=SWAGGER_URL)
 
-# testing variables
-payload_data = {
-    "username": "Jessica Temporal",
-    "password": "JessPass"
-}
-
 
 # application routes
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
     '''Home page'''
     try:
-        # rows = DBConnection.selectRows(CUSTOMER_TABLE)
-        # session[USERNAME] = payload_data['username']
-        # session[TOKEN] = Token.generateToken(payload_data, SECRET_KEY).decode('latin-1')
-        # logger.info(f'Generated token {session[TOKEN]} for customer {session[USERNAME]}')
-        # return f"{session[TOKEN]} {rows}"
-        return redirect(url_for('login'))
+        if not session.get('username', None):
+            return redirect(url_for('login'))
+        return render_template('home.html',  title='Home', id='nav1', username=session['username'], token=session['token'])
     except Exception as e:
         logger.exception(f'Error while accessing home for {session.get(USERNAME, "no user")}')
-        return f"{e}"
+        return f"{'message': 'server error'}"
 
 
 @app.route('/signup', methods=['GET','POST'])
@@ -57,10 +46,9 @@ def signup():
             form.first_name.data = ''
             form.last_name.data = ''
             form.address.data = ''
-            form.mobile_number.data = -1
-            # unable to register customer
+            form.mobile_number.data = ''
             if not res[0]:
-                logger.warning(res[1])
+                # unable to register customer
                 return render_template('signup_form.html', title='Signup', form=form, id='nav3', msg=res[1])
             # customer registered, proceed to login
             logger.info(res[1])
@@ -68,18 +56,34 @@ def signup():
         return render_template('signup_form.html', title='Signup', form=form, id='nav3')
     except Exception as e:
         logger.exception('Error while accessing signup')
-        return f"{e}"
+        return render_template('signup_form.html', title='Signup', form=form, id='nav3', msg='Error while accessing signup')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     '''Login Page'''
     try:
         form = LoginForm()
+        if form.validate_on_submit():
+            res = tryToLoginCustomer(form)
+            if not res[0]:
+                # Could not log in
+                return render_template('login_form.html', title='Login', form=form, id='nav3', msg=res[1])
+            # Valid credentials
+            customer = res[2]
+            res = prepareCustomerForLogin(customer)
+            if not res[0]:
+                # Could not authenticate
+                return render_template('login_form.html', title='Login', form=form, id='nav3', msg=res[1])
+            # Login successful
+            return redirect(url_for('home'))
         return render_template('login_form.html', title='Login', form=form, id='nav3')
     except Exception as e:
         logger.exception(f'Error while accessing login. User: {session.get(USERNAME, "no user")}')
-        return f"{e}"
+        return render_template('login_form.html', title='Login', form=form, id='nav3', msg=f'An error during login')
+
+
+# @app.route()
 
 
 @app.route('/usersession/<some_val>', methods =['GET'])
@@ -96,14 +100,14 @@ def getUserSession(some_val):
 def clearsession():
     '''Page to clear session variables'''
     try:
-        logger.info(f'Clearing session variables for user {session[USERNAME]}')
+        logger.info(f"Clearing session variables for user {session[USERNAME]}")
         session.pop(USERNAME, default=None)
         session.pop(TOKEN, default=None)
         logger.info(f'Session variables cleared')
-        return f"Cleared. Token is now {session.get(TOKEN, 'No token')}"
+        return redirect(url_for('login'))
     except Exception as e:
         logger.exception(f'Error while clearing session')
-        return f"{e}"
+        return redirect(url_for('login'))
 
 
 # API methods
@@ -118,7 +122,17 @@ def retrieveToken():
             logger.warning(f'Bad request while retrieving api token. username length: {len(username)}. password length: {len(password)}. payload length: {len(payload)}')
             return make_response(jsonify({'token' : '', 
                                           'success' : False}), 400)
-        token = Token.generateToken(payload, SECRET_KEY).decode('latin-1')
+        # form to check authorization
+        form = CustomerForm()
+        form.username.data = username
+        form.password.data = password
+        res = tryToLoginCustomer(form)
+        if not res[0]:
+            logger.warning(f'Invalid credentials: Username {username}, Password {password}')
+            return make_response(jsonify({'token' : '', 
+                                          'success' : False}), 400)
+        payload['timestamp'] = f"{datetime.now()}"
+        token = Token.generateToken(payload, SECRET_KEY).decode('utf-8')
         if not token:
             logger.warning(f'Unable to generate token for {payload}')
             return make_response(jsonify({'token' : token, 
